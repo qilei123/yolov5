1e67fbeed113e454ab2be2c37b9c42cfdd64ce36
qilei123 <qileimail123@gmail.com>
2022-02-22 02:42:48 -0500

    just syn

--- utils/general.py
+++ utils/general.py
@@ -579,13 +579,22 @@ def xyxy2xywh(x):
     return y
 
 
-def xywh2xyxy(x):
+def xywh2xyxy(x):#Here in (xywh), xy is the center of the bounding box
     # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
     y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
     y[:, 0] = x[:, 0] - x[:, 2] / 2  # top left x
     y[:, 1] = x[:, 1] - x[:, 3] / 2  # top left y
     y[:, 2] = x[:, 0] + x[:, 2] / 2  # bottom right x
     y[:, 3] = x[:, 1] + x[:, 3] / 2  # bottom right y
+    return y
+
+def xywh2xyxy1(x):#Here in (xywh), xy is the left-top point of the bounding box
+    # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
+    y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
+    y[:, 0] = x[:, 0]  # top left x
+    y[:, 1] = x[:, 1]  # top left y
+    y[:, 2] = x[:, 0] + x[:, 2] # bottom right x
+    y[:, 3] = x[:, 1] + x[:, 3]  # bottom right y
     return y
 
 
