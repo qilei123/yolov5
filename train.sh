b056fc8de64f26afddd8b7dea70929a3087f9268
qilei123 <qileimail123@gmail.com>
2022-02-24 17:59:24 -0500

    just syn

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=1
-python train.py --img 1920 --batch 8 --epochs 64 --data trans_drone_cat3.yaml --weights yolov5s.pt --project /data3/qilei_chen/work_model_dirs/yolov5/trans_drone_cat3/yolov5s
+python train.py --img 1920 --batch 8 --epochs 64 --data trans_drone_cat3.yaml --weights yolov5s.pt --project /data3/qilei_chen/work_model_dirs/yolov5/trans_drone_cat3/yolov5s --name exp_1920_mosaic4
