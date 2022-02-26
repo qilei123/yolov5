b8c09227e0f7c3bb52d5f5782064143e93842bb7
qilei123 <qileimail123@gmail.com>
2022-02-26 00:17:54 -0500

    just syn

--- utils/setpath.py
+++ utils/setpath.py
@@ -3,6 +3,7 @@ import sys,os
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]  # YOLOv5 root directory
+print(ROOT)
 if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add ROOT to PATH
 ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
