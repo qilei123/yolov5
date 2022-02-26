bd602d70981ccee6e61afbcf33874ed339cfe910
qilei123 <qileimail123@gmail.com>
2022-02-26 01:02:53 -0500

    just syn

--- utils/autoanchor.py
+++ utils/autoanchor.py
@@ -114,7 +114,7 @@ def kmean_anchors(dataset='./data/coco128.yaml', n=9, img_size=640, thr=4.0, gen
             data_dict = yaml.safe_load(f)  # model dict
         from utils.datasets import LoadImagesAndLabels,LoadImagesAndLabels4COCO
         if 'obb' in data_dict['train']:
-            dataset = LoadImagesAndLabels(os.path.join(data_dict['path'],data_dict['train']), augment=True, rect=True)
+            dataset = LoadImagesAndLabels4COCO(os.path.join(data_dict['path'],data_dict['train']), augment=True, rect=True)
         else:
             dataset = LoadImagesAndLabels(data_dict['train'], augment=True, rect=True)
 
