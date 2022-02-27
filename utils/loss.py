d4b72ce983fb703bff12f8f36ff588d7b5a993b8
qilei123 <qileimail123@gmail.com>
2022-02-27 03:41:20 -0500

    just syn

--- utils/loss.py
+++ utils/loss.py
@@ -167,6 +167,7 @@ class ComputeLoss:
         return (lbox + lobj + lcls) * bs, torch.cat((lbox, lobj, lcls)).detach()
 
     def build_targets(self, p, targets):
+        print(p.shape)
         # Build targets for compute_loss(), input targets(image,class,x,y,w,h)
         na, nt = self.na, targets.shape[0]  # number of anchors, targets
         tcls, tbox, indices, anch = [], [], [], []
