a43c99f3110761784c7020b2f8d4f06e2b7a087f
qilei123 <qileimail123@gmail.com>
2022-02-28 15:06:28 -0500

    just syn

--- train.sh
+++ train.sh
@@ -5,4 +5,4 @@ git pull
 #python train.py --img 1920 --batch 8 --epochs 64 --data trans_drone_cat3.yaml --weights yolov5s.pt --project /data3/qilei_chen/work_model_dirs/yolov5/trans_drone_cat3/yolov5s --name exp_1920_mosaic4
 #python train.py --img 1920 --batch 8 --epochs 64 --data trans_drone_cat3.yaml --weights yolov5s.pt --project /data3/qilei_chen/work_model_dirs/yolov5/trans_drone_cat3/yolov5s --name exp_1920_mosaic4
 
-python train.py --exist-ok --device 1 --img 1280 --batch 1 --epochs 64 --data trans_drone_cat3.yaml --weights yolov5s.pt --project /data/qilei/work_dir_models_results/yolov5/trans_drone_cat3/yolov5s --name exp_1280_mosaic4_debug #--mosaic_n False
+python train.py --exist-ok --device 1 --img 1280 --batch 2 --epochs 64 --data trans_drone_cat3.yaml --weights yolov5s.pt --project /data/qilei/work_dir_models_results/yolov5/trans_drone_cat3/yolov5s --name exp_1280_mosaic4_debug #--mosaic_n False
