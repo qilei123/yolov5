2750e1c1a6951b2b318e741b89ba586a5af1de9f
qilei123 <qileimail123@gmail.com>
2022-03-03 16:05:49 -0500

    just syn

--- utils/general.py
+++ utils/general.py
@@ -653,6 +653,7 @@ def resample_segments(segments, n=1000):
         x = np.linspace(0, len(s) - 1, n)
         xp = np.arange(len(s))
         segments[i] = np.concatenate([np.interp(x, xp, s[:, i]) for i in range(2)]).reshape(2, -1).T  # segment xy
+        print(segments[i])
     return segments
 
 
