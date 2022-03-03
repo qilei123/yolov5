811c10150ef5ba6321c7ea4a6b4dd0705f5b10e3
qilei123 <qileimail123@gmail.com>
2022-03-03 15:17:44 -0500

    just syn

--- utils/datasets.py
+++ utils/datasets.py
@@ -1199,6 +1199,7 @@ class LoadImagesAndLabels4COCO(LoadImagesAndLabels):
 
 if __name__ == "__main__":
     hyp = yaml.safe_load('data/hyps/hyp.scratch.yaml')
+    print(hyp)
     dataset_coco = LoadImagesAndLabels4COCO('/home/qilei/DATASETS/trans_drone/andover_worster/annotations/test_AW_obb.json',augment = True,hyp=hyp)
     _,label_outs,_,_ = dataset_coco.__getitem__(11)
     print(label_outs)
