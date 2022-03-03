df7102ddcc2920a6a74c879263f1af9f8f8dceeb
qilei123 <qileimail123@gmail.com>
2022-03-03 15:13:48 -0500

    just syn

--- utils/datasets.py
+++ utils/datasets.py
@@ -1198,6 +1198,6 @@ class LoadImagesAndLabels4COCO(LoadImagesAndLabels):
             pbar.close()    
 
 if __name__ == "__main__":
-    dataset_coco = LoadImagesAndLabels4COCO('/home/qilei/DATASETS/trans_drone/andover_worster/annotations/test_AW_obb.json')
+    dataset_coco = LoadImagesAndLabels4COCO('/home/qilei/DATASETS/trans_drone/andover_worster/annotations/test_AW_obb.json',augment = True)
     _,label_outs,_,_ = dataset_coco.__getitem__(11)
     print(label_outs)
