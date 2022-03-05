d3924f2c8be8925946c66301234ed176525bd54a
qilei123 <qileimail123@gmail.com>
2022-03-05 16:53:42 -0500

    just syn

--- utils/loss.py
+++ utils/loss.py
@@ -273,7 +273,7 @@ class ComputeLossOBB:
                 # Regression
                 pxy = ps[:, :2].sigmoid() * 2 - 0.5
                 pwh = (ps[:, 2:4].sigmoid() * 2) ** 2 * anchors[i]
-                ptheta =ps[:, 4].sigmoid()
+                ptheta =ps[:, 4].sigmoid() * pi
                 ptheta = torch.unsqueeze(ptheta,1)*(pi/(-2))
                 pbox = torch.cat((pxy, pwh, ptheta), 1)  # predicted box
                 print(pbox.shape)
