202cc3849d48bc5c0ce21533c63bf61f97a0f847
qilei123 <qileimail123@gmail.com>
2022-03-07 13:06:56 -0500

    just syn

--- models/yolo_obb.py
+++ models/yolo_obb.py
@@ -61,12 +61,11 @@ class DetectOBB(nn.Module):
                 if self.inplace:
                     y[..., 0:2] = (y[..., 0:2] * 2 - 0.5 + self.grid[i]) * self.stride[i]  # xy
                     y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
-                    y[..., 4] =y[:, 4].sigmoid() * 3.1415926/(-2)
+                    y[..., 4:5] =y[:, 4:5].sigmoid() * 3.1415926/(-2)
                 else:  # for YOLOv5 on AWS Inferentia https://github.com/ultralytics/yolov5/pull/2953
                     xy = (y[..., 0:2] * 2 - 0.5 + self.grid[i]) * self.stride[i]  # xy
                     wh = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
-                    theta = y[:, 4].sigmoid() * 3.1415926/(-2)
-                    theta = torch.unsqueeze(theta,1)
+                    theta = y[:, 4:5].sigmoid() * 3.1415926/(-2)
                     y = torch.cat((xy, wh, theta,y[..., 5:]), -1)
                 z.append(y.view(bs, -1, self.no))
         #exit(0)
