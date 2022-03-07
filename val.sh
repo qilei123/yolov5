0b2305e1f933fe1f5772c63e669711301cae48da
qilei123 <qileimail123@gmail.com>
2022-03-07 17:07:18 -0500

    just syn

--- val.sh
+++ val.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 git pull
 #python val.py --weights /data3/qilei_chen/work_model_dirs/yolov5/trans_drone_cat3/yolov5s/exp/weights/best.pt --data trans_drone_cat3.yaml --img 1280 --task test
 
