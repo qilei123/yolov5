5fc6a24ea628642624846e0d8f904bc42f9c1e5f
qilei123 <qileimail123@gmail.com>
2022-03-07 11:25:04 -0500

    just syn

--- utils/datasets.py
+++ utils/datasets.py
@@ -1287,7 +1287,8 @@ class LoadImagesAndLabels4OBB(LoadImagesAndLabels4COCO):
         if nl:
             labels[:, 1:5] = xyxy2xywhn(labels[:, 1:5], w=img.shape[1], h=img.shape[0], clip=True, eps=1E-3)
             segments4 = np.array([xy2xyn(x, w=img.shape[1], h=img.shape[0],clip=True, eps=1E-3) for x in segments4])
-            
+            print(labels.shape)
+            print(segments4.shape)  
         if self.augment:
             # Albumentations
             img, labels = self.albumentations(img, labels)
