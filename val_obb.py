fdf6890df53687c60752477392088e4e422496cc
qilei123 <qileimail123@gmail.com>
2022-03-07 10:51:10 -0500

    just syn

--- val_obb.py
+++ val_obb.py
@@ -354,6 +354,7 @@ def main(opt):
     if opt.task in ('train', 'val', 'test'):  # run normally
         if opt.conf_thres > 0.001:  # https://github.com/ultralytics/yolov5/issues/1466
             LOGGER.info(f'WARNING: confidence threshold {opt.conf_thres} >> 0.001 will produce invalid mAP values.')
+        print(opt)
         run(**vars(opt))
 
     else:
