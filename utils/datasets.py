6395af2afa4ae4967d23f971063431ac78c13d55
qilei123 <qileimail123@gmail.com>
2022-03-07 12:17:43 -0500

    just syn

--- utils/datasets.py
+++ utils/datasets.py
@@ -1206,6 +1206,7 @@ class LoadImagesAndLabels4COCO(LoadImagesAndLabels):
             self.img_files = [self.img_files[i] for i in irect]
             #self.label_files = [self.label_files[i] for i in irect]
             self.labels = [self.labels[i] for i in irect]
+            self.segments = [self.segments[i] for i in irect]
             self.shapes = s[irect]  # wh
             ar = ar[irect]
 
