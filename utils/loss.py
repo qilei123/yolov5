6724c36b0fce01b2fb5111165c03af3502abc6de
qilei123 <qileimail123@gmail.com>
2022-03-07 01:45:34 -0500

    just syn

--- utils/loss.py
+++ utils/loss.py
@@ -288,7 +288,7 @@ class ComputeLossOBB:
                 print(iou.shape)
                 #with torch.no_grad():
                 #    iou = bt.bbox_overlaps(pbox.cpu(), tbox[i].cpu())
-                iou = obb_overlaps(pbox, tbox[i])
+                iou = obb_overlaps(pbox, tbox[i],is_aligned=True)
                 print(iou.shape)
 
                 lbox += (1.0 - iou).mean()  # iou loss
