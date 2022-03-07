da5b8203a61935459a6f9f81535b2617b3a78008
qilei123 <qileimail123@gmail.com>
2022-03-07 13:17:15 -0500

    just syn

--- models/yolo_obb.py
+++ models/yolo_obb.py
@@ -63,7 +63,7 @@ class DetectOBB(nn.Module):
                     y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
                     theta =y[..., 4:5]* 3.1415926/(-2)
                     print(theta.shape)
-                    y[...,4] = theta
+                    y[...,4:5] = theta
                 else:  # for YOLOv5 on AWS Inferentia https://github.com/ultralytics/yolov5/pull/2953
                     xy = (y[..., 0:2] * 2 - 0.5 + self.grid[i]) * self.stride[i]  # xy
                     wh = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
