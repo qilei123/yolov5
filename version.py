fc83e84c085c32612d6a2be3ec6a07c3761b40be
qilei123 <qileimail123@gmail.com>
2022-03-08 01:56:34 -0500

    just syn

--- version.py
+++ version.py
@@ -1,3 +1,6 @@
-__version__ = '2.2.0+9a912f4'
-short_version = '2.2.0'
-version_info = (2, 2, 0)
+# GENERATED VERSION FILE
+# TIME: Tue Mar  8 01:52:57 2022
+
+__version__ = '0.0.2+09d304b'
+short_version = '0.0.2'
+version_info = (0, 0, 2)
