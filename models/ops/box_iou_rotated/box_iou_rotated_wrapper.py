59ce34e2ccb457b77c54f1c2d9a86ac3599312af
qilei123 <qileimail123@gmail.com>
2022-03-09 23:50:42 -0500

    just syn

--- models/ops/box_iou_rotated/box_iou_rotated_wrapper.py
+++ models/ops/box_iou_rotated/box_iou_rotated_wrapper.py
@@ -35,15 +35,19 @@ def obb_overlaps(bboxes1, bboxes2, mode='iou', is_aligned=False, device_id=None)
         outputs = box_iou_rotated_ext.overlaps(
             bboxes1_th, bboxes2_th, mode == 'iou')
         # same bug will happen when bbox size is to small
-        '''
-        too_small1 = bboxes1_th[:, [2, 3]].min(1)[0] < 0.001
-        too_small2 = bboxes2_th[:, [2, 3]].min(1)[0] < 0.001
-        if too_small1.any() or too_small2.any():
-            inds1 = torch.nonzero(too_small1, as_tuple=False)
-            inds2 = torch.nonzero(too_small2, as_tuple=False)
-            outputs[inds1, :] = 0.
-            outputs[:, inds2] = 0.
-        '''
+        
+        try:
+            too_small1 = bboxes1_th[:, [2, 3]].min(1)[0] < 0.001
+            too_small2 = bboxes2_th[:, [2, 3]].min(1)[0] < 0.001
+            if too_small1.any() or too_small2.any():
+                inds1 = torch.nonzero(too_small1, as_tuple=False)
+                inds2 = torch.nonzero(too_small2, as_tuple=False)
+                outputs[inds1, :] = 0.
+                outputs[:, inds2] = 0.
+        except:
+            print(bboxes1_th)
+            print(bboxes2_th)
+        
     if is_numpy:
         outputs = outputs.cpu().numpy()
     return outputs
