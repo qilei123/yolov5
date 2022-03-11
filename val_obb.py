0007c4ef747d6a0b3f558876fc8b6feddaf8303d
qilei123 <qileimail123@gmail.com>
2022-03-11 17:48:15 -0500

    just syn

--- val_obb.py
+++ val_obb.py
@@ -84,11 +84,11 @@ def process_batch(detections, labels, iouv):
     #print(labels)
     #print(detections)
     iou = obb_overlaps(labels[:, 1:], detections[:, :5])
-    print(iou)
-    print(torch.max(iou,1))
+    #print(iou)
+    #print(torch.max(iou,1))
     x = torch.where((iou >= iouv[0]) & (labels[:, 0:1] == detections[:, 6]))  # IoU above threshold and classes match
-    print(x)
-    exit(0)
+    #print(x)
+    #exit(0)
     if x[0].shape[0]:
         matches = torch.cat((torch.stack(x, 1), iou[x[0], x[1]][:, None]), 1).cpu().numpy()  # [label, detection, iou]
         if x[0].shape[0] > 1:
