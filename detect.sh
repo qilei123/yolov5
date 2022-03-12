d2b1a8b904580ccf62c576a731d0134c59112803
qilei123 <qileimail123@gmail.com>
2022-03-11 23:50:57 -0500

    just syn

--- detect.sh
+++ detect.sh
@@ -2,4 +2,4 @@ python detect.py \
     --source '/data/qilei/DATASETS/trans_drone/images/DJI_0003 400 90 degree 29.jpg' \
     --imgsz 1280 \
     --weights /data/qilei/work_dir_models_results/yolov5/trans_drone_cat3/yolov5s_obb/exp_1280_mosaic4_debug/weights/last.pt \
-    --data trans_drone_cat3.yaml
+    --data data/trans_drone_cat3.yaml
