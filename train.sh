4b695aac59306c2af918d15a33f852323ebb412e
qilei123 <qileimail123@gmail.com>
2022-03-14 01:49:19 -0400

    just syn

--- train.sh
+++ train.sh
@@ -7,6 +7,6 @@ root_dir2=/data3/qilei_chen
 #python train.py --img 1920 --batch 8 --epochs 64 --data trans_drone_cat3.yaml --weights yolov5s.pt --project /data3/qilei_chen/work_model_dirs/yolov5/trans_drone_cat3/yolov5s --name exp_1920_mosaic4
 #python train.py --img 1920 --batch 8 --epochs 64 --data trans_drone_cat3.yaml --weights yolov5s.pt --project /data3/qilei_chen/work_model_dirs/yolov5/trans_drone_cat3/yolov5s --name exp_1920_mosaic4
 
-python train_obb.py --exist-ok --device 0 --img 1280 --batch 32 --epochs 64 --hyp data/hyps/hyp.scratch_td.yaml --cfg models/yolov5s_obb.yaml --data trans_drone_cat3.yaml --weights yolov5s.pt --project $root_dir1/work_dir_models_results/yolov5/trans_drone_cat3/yolov5s_obb --name exp_1280_mosaic4_debug #--mosaic_n False
+python train_obb.py --exist-ok --device 0 --img 1280 --batch 1 --epochs 64 --hyp data/hyps/hyp.scratch_td.yaml --cfg models/yolov5s_obb.yaml --data trans_drone_cat3.yaml --weights yolov5s.pt --project $root_dir1/work_dir_models_results/yolov5/trans_drone_cat3/yolov5s_obb --name exp_1280_mosaic4_debug #--mosaic_n False
 
 #python train.py --exist-ok --device 1 --img 1280 --batch 4 --epochs 64 --cfg models/yolov5s.yaml --data trans_drone_cat3.yaml --weights yolov5s.pt --project /data/qilei/work_dir_models_results/yolov5/trans_drone_cat3/yolov5s_obb --name exp_1280_mosaic4_debug #--mosaic_n False
