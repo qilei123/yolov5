8ecbf402808b21acdb9a4fff5754c3a93b90ca3a
qilei123 <qileimail123@gmail.com>
2022-03-14 02:26:23 -0400

    just syn

--- train_obb.py
+++ train_obb.py
@@ -237,7 +237,7 @@ def train(hyp,  # path/to/hyp.yaml or hyp dictionary
     if RANK in [-1, 0]:
         val_loader = create_dataloader_obb(val_path, imgsz, batch_size // WORLD_SIZE * 2, gs, single_cls,
                                        hyp=hyp, cache=None if noval else opt.cache,
-                                       rect=True, rank=-1, workers=workers * 2, pad=0.5,
+                                       rect=True, rank=-1, workers=workers * 2, pad=0.0,
                                        prefix=colorstr('val: '))[0]
 
         if not resume:
