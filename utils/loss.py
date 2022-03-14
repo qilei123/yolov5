fa5fec9c85a5d013c73bf3008b56fe34799d0da3
qilei123 <qileimail123@gmail.com>
2022-03-14 17:20:25 -0400

    just syn

--- utils/loss.py
+++ utils/loss.py
@@ -338,7 +338,7 @@ class ComputeLossOBB:
         g = 0.5  # bias
         off = torch.tensor([[0, 0],
                             [1, 0], [0, 1], [-1, 0], [0, -1],  # j,k,l,m
-                             [1, 1], [1, -1], [-1, 1], [-1, -1],  # jk,jm,lk,lm
+                             #[1, 1], [1, -1], [-1, 1], [-1, -1],  # jk,jm,lk,lm
                             ], device=targets.device).float() * g  # offsets
 
         for i in range(self.nl):
@@ -354,15 +354,19 @@ class ComputeLossOBB:
                 r = t[:, :, 4:6] / anchors[:, None]  # wh ratio
                 j = torch.max(r, 1 / r).max(2)[0] < self.hyp['anchor_t']  # compare
                 # j = wh_iou(anchors, t[:, 4:6]) > model.hyp['iou_t']  # iou(3,n)=wh_iou(anchors(3,2), gwh(n,2))
+                o_t_l = t.shape[0]
                 t = t[j]  # filter
-
+                f_t_l = t.shape[0]
+                print(o_t_l)
+                print(f_t_l)
+                print(f_t_l/o_t_l)
                 # Offsets
                 gxy = t[:, 2:4]  # grid xy
                 gxi = gain[[2, 3]] - gxy  # inverse
                 j, k = ((gxy % 1 < g) & (gxy > 1)).T
                 l, m = ((gxi % 1 < g) & (gxi > 1)).T
                 j = torch.stack((torch.ones_like(j), j, k, l, m))
-                t = t.repeat((9, 1, 1))[j]
+                t = t.repeat((5, 1, 1))[j]
                 offsets = (torch.zeros_like(gxy)[None] + off[:, None])[j]
             else:
                 t = targets[0]
