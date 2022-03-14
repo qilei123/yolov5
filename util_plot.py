382bb62d19abd36e42e69911cac14b26eed9e40a
qilei123 <qileimail123@gmail.com>
2022-03-14 12:10:15 -0400

    just syn

--- util_plot.py
+++ util_plot.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 def show_segs(img,segs,save_dir=None):
     for seg in segs:
+        print(seg)
         pts = np.array(seg, np.int32)
         pts = pts.reshape((-1,1,2))
         cv2.polylines(img,[pts],True,(0,255,255))     
